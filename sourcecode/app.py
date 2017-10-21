717e81f098a11e3d604d8e6c10e5b7c0d1fd8716
DestelloVeloz <40174677@live.napier.ac.uk>
2017-10-21 12:41:31 +0100

    Images organised in subcategories and templates moved to sourcecode

--- sourcecode/app.py
+++ sourcecode/app.py
@@ -2,13 +2,17 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def root():
-    return render_template('index.html'), 200
+def home():
+    return render_template('index.html')
 
 @app.route('/status/<currstatus>')
 
 def status(currstatus):
     return "Pokemon Status"
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return "Opps, the URL you requested for does not exist",404
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=9116, debug=True)
